4479077e7cd335ed5bd3743e1d802ce20bedfe31
FannarHrafn <fannarhrafn@gmail.com>
2018-05-07 12:28:56 +0000

    Add files via upload

---
+++ js/filter-tags.js
@@ -0,0 +1,49 @@
+(function() {
+
+  var $links = $('#gallery a');                    // Store all links
+  var $buttons = $('#buttons');                   // Store buttons element
+  var tagged = {};                                // Create tagged object
+
+  $links.each(function() {                         // Loop through images and
+    var link = this;                               // Store link in variable
+    var tags = $(this).data('tags');              // Get this element's tags
+
+    if (tags) {                                   // If the element had tags
+      tags.split(',').forEach(function(tagName) { // Split at comma and
+        if (tagged[tagName] == null) {            // If object doesn't have tag
+          tagged[tagName] = [];                   // Add empty array to object
+        }
+        tagged[tagName].push(link);                // Add the image to the array
+      });
+    }
+  });
+
+  $('<button/>', {                                 // Create empty button
+    text: 'Show All',                              // Add text 'show all'
+    class: 'active',                               // Make it active
+    click: function() {                            // Add onclick handler to
+      $(this)                                      // Get the clicked on button
+        .addClass('active')                        // Add the class of active
+        .siblings()                                // Get its siblings
+        .removeClass('active');                    // Remove active from siblings
+      $links.show();                                // Show all images
+    }
+  }).appendTo($buttons);                           // Add to buttons
+
+  $.each(tagged, function(tagName) {               // For each tag name
+    $('<button/>', {                               // Create empty button
+      text: tagName + ' (' + tagged[tagName].length + ')', // Add tag name
+      click: function() {                          // Add click handler
+        $(this)                                    // The button clicked on
+          .addClass('active')                      // Make clicked item active
+          .siblings()                              // Get its siblings
+          .removeClass('active');                  // Remove active from siblings
+        $links                                      // With all of the images
+          .hide()                                  // Hide them
+          .filter(tagged[tagName])                 // Find ones with this tag
+          .show();                                 // Show just those images
+      }
+    }).appendTo($buttons);                         // Add to the buttons
+  });
+
+}());
